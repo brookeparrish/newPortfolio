1f219cdf9eb7171b96312ec45d5f63ffb67b8f6a
Brooke <parrish.e.brooke@gmail.com>
2020-05-16 19:12:25 -0700

    added about and footer components

--- portfolio/src/components/about/about.js
+++ portfolio/src/components/about/about.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
+
 function About() {
     return (
-        <div>About</div>
+        <div class="jumbotron jumbotron-fluid text-center">
+            <div class="container">
+                <h1 class="display-4">Brooke Parrish</h1>
+                <p class="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed libero enim sed faucibus turpis in eu. In pellentesque massa placerat duis ultricies lacus. Diam maecenas ultricies mi eget mauris pharetra et ultrices neque. Iaculis nunc sed augue lacus viverra vitae. Fringilla est ullamcorper eget nulla facilisi etiam. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum. Tristique senectus et netus et malesuada fames. Nisl nisi scelerisque eu ultrices vitae. Proin fermentum leo vel orci. Varius duis at consectetur lorem donec massa sapien faucibus et.</p>
+            </div>
+        </div>
     );
 }
 
--- portfolio/src/components/footer/footer.js
+++ portfolio/src/components/footer/footer.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import './Footer.css';
 
 function Footer() {
     return (
-        <div>Footer</div>
+        <nav class="navbar fixed-bottom navbar-light bg-light d-block text-center">
+                <a href="https://www.linkedin.com/in/brookeparrish/">
+                    <i class="fab fa-linkedin"></i>
+                </a>
+                <a class="pl-1" href="https://www.github.com/brookeparrish/">
+                    <i class="fab fa-github"></i>
+                </a>
+        </nav>
     );
 }
 
