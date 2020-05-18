997444383df0b76ff9a11c14fe313a60071eb7dc
Brooke <parrish.e.brooke@gmail.com>
2020-05-17 17:36:24 -0700

    added photo

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -4,11 +4,13 @@ import About from './components/About/About';
 import Projects from './components/Projects/Projects';
 import ContactUs from './components/ContactUs/ContactUs';
 import Footer from './components/Footer/Footer';
+import Photo from './components/Photo/Photo';
 
 
 function App() {
   return (
     <div>
+      <Photo />
       <About />
       <Projects />
       <ContactUs />
--- portfolio/src/components/Projects/Projects.js
+++ portfolio/src/components/Projects/Projects.js
@@ -14,8 +14,8 @@ function Projects() {
                             <h5 className="card-title">Card title</h5>
                             <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="card-link">Card link</a>
-                            <a href="#" className="card-link">Another link</a>
+                            <a href="# " className="card-link">Card link</a>
+                            <a href="# " className="card-link">Another link</a>
                         </div>
                     </div>
 
@@ -24,8 +24,8 @@ function Projects() {
                             <h5 className="card-title">Card title</h5>
                             <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="card-link">Card link</a>
-                            <a href="#" className="card-link">Another link</a>
+                            <a href="# " className="card-link">Card link</a>
+                            <a href="# " className="card-link">Another link</a>
                         </div>
                     </div>
                 </div>
@@ -38,8 +38,8 @@ function Projects() {
                             <h5 className="card-title">Card title</h5>
                             <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="card-link">Card link</a>
-                            <a href="#" className="card-link">Another link</a>
+                            <a href="# " className="card-link">Card link</a>
+                            <a href="# " className="card-link">Another link</a>
                         </div>
                     </div>
 
@@ -48,8 +48,8 @@ function Projects() {
                             <h5 className="card-title">Card title</h5>
                             <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
                             <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" className="card-link">Card link</a>
-                            <a href="#" className="card-link">Another link</a>
+                            <a href="# " className="card-link">Card link</a>
+                            <a href="# " className="card-link">Another link</a>
                         </div>
                     </div>
                 </div>
