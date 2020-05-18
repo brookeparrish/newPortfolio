668a263c967394989c062b9be1ba039078fcba3a
Brooke <parrish.e.brooke@gmail.com>
2020-05-17 19:41:21 -0700

    made icons larger

--- portfolio/src/components/footer/footer.js
+++ portfolio/src/components/footer/footer.js
@@ -3,12 +3,12 @@ import './Footer.css';
 
 function Footer() {
     return (
-        <nav className="navbar fixed-bottom navbar-light bg-light d-block text-center">
+        <nav className="navbar sticky-bottom navbar-light bg-light d-block text-center">
                 <a href="https://www.linkedin.com/in/brookeparrish/" target="_blank">
-                    <i className="fab fa-linkedin"></i>
+                    <i className="fab fa-linkedin fa-2x"></i>
                 </a>
                 <a className="pl-1" href="https://www.github.com/brookeparrish/" target="_blank">
-                    <i className="fab fa-github"></i>
+                    <i className="fab fa-github fa-2x"></i>
                 </a>
         </nav>
     );
