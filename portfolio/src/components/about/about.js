1136c4197dcde373a99ac518b8111ebe20d76847
Brooke <parrish.e.brooke@gmail.com>
2020-05-17 19:32:01 -0700

    added css

--- portfolio/src/components/about/about.js
+++ portfolio/src/components/about/about.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
+import '../About/About.css';
 
 function About() {
     return (
         <div className="jumbotron jumbotron-fluid text-center">
             <div className="container">
                 <h1 className="display-4">Brooke Parrish</h1>
-                <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed libero enim sed faucibus turpis in eu. In pellentesque massa placerat duis ultricies lacus. Diam maecenas ultricies mi eget mauris pharetra et ultrices neque. Iaculis nunc sed augue lacus viverra vitae. Fringilla est ullamcorper eget nulla facilisi etiam. Pretium vulputate sapien nec sagittis aliquam malesuada bibendum. Tristique senectus et netus et malesuada fames. Nisl nisi scelerisque eu ultrices vitae. Proin fermentum leo vel orci. Varius duis at consectetur lorem donec massa sapien faucibus et.</p>
+                <p className="lead">I am a tech enthusiast with over 10 years experience with working on a team. I am a self-motivated, multitasker who is eager to learn. I have a background in customer service, providing new patient consultations in the dental field and also the opposite side of the spectrum at a dental lab.</p>
+
+                <p className="lead">When I am not focusing on moving forward in the tech world, I enjoy seeing live music, dancing to country songs and staying active. </p>
             </div>
         </div>
     );
