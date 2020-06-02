a8457fc7a4e94475564d625a0872f18d64ac2487
Brooke <parrish.e.brooke@gmail.com>
2020-06-01 22:13:25 -0700

    added github pages info

--- portfolio/src/components/Projects/Projects.js
+++ portfolio/src/components/Projects/Projects.js
@@ -49,16 +49,6 @@ function Projects() {
                             <a href="https://proj-3-options.herokuapp.com/" target="_blank" className="card-link">Visit Website</a>
                         </div>
                     </div>
-
-                    <div className="card text-center" style={cardStyle}>
-                        <div className="card-body">
-                            <h5 className="card-title">Card title</h5>
-                            <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="# " className="card-link">Card link</a>
-                            <a href="# " className="card-link">Another link</a>
-                        </div>
-                    </div>
                 </div>
             </div>
         </div>
